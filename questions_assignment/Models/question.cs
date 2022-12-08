6ce8394bbde6a5c75bee4ce0118ccb8a78c3f801
Swanitha73 <swanithathumma@gmail.com>
2022-12-07 19:17:05 -0600

    Added API

---
+++ questions_assignment/Models/question.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace questions_assignment.Models
+{
+    public class question
+    {
+        public int operand1 { get; set; }
+
+        public int operand2 { get; set; }
+
+        public string oper { get; set; }
+
+        public int answer { get; set; }
+
+        public int randanswer1 { get; set; }
+
+        public int randanswer2 { get; set; }
+    }
+}
